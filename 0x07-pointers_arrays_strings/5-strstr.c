5afa5fa57fa70072695a2a5af2e45304fea71ce3
Megmello1 <mackatiah107@gmail.com>
2023-05-08 12:32:12 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *g = haystack;
+char *h = needle;
+while (*g == *h && *h != '\0')
+{
+g++;
+h++;
+}
+if (*h == '\0')
+return (haystack);
+}
+return (0);
+}
